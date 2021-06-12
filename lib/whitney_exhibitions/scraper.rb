19108a94c53b21bb32b0a1e4b16aaf60e0046f45
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-06-12 04:02:11 +0000

    updated exhibition scraper to separate lists

--- lib/whitney_exhibitions/scraper.rb
+++ lib/whitney_exhibitions/scraper.rb
@@ -5,7 +5,11 @@ class WhitneyExhibitions::Scraper
     uri = "https://whitney.org/exhibitions"
     doc = Nokogiri::HTML(URI.open(uri))
     
-    exhibitions = doc.css("div.exhibitions__list h2") + doc.css("div.exhibitions__list h3") #minus first two outputs
+    #exhibitions = doc.css("div.exhibitions__list h2") + doc.css("div.exhibitions__list h3") #minus first two outputs
+
+    list_one = doc.css("div.exhibitions__list h2") 
+    list_two = doc.css("div.exhibitions__list h3")
+    exhibitions = list_one + list_two
     
     exhibitions.each do |e|
       name = e.text.strip
