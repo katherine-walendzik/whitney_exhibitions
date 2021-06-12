3d54967abcf4ce1df12a6a2aa05d1dd642d9f887
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-06-12 01:11:45 +0000

    scraping notes

--- lib/whitney_exhibitions/scraper.rb
+++ lib/whitney_exhibitions/scraper.rb
@@ -3,9 +3,8 @@ class WhitneyExhibitions::Scraper
   def self.scrape_exhibitions
     uri = "https://whitney.org/exhibitions"
     doc = Nokogiri::HTML(URI.open(uri))
-    exhibitions = doc.css("div.exhibitions__list").text
-    second_exhibitions = doc.css("div.exhibitions__list h3").text.rstrip
-    binding.pry
+    exhibitions = doc.css("div.exhibitions__list h3").text.rstrip 
+    second_exhibitions = doc.css("div.exhibitions__list h3").text.rstrip #minus first two outputs
   end
   
 end
