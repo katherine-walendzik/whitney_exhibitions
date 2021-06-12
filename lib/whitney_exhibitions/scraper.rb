d2295887a5b2adcccd4a0ac6843fa98532dade8a
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-06-12 00:52:29 +0000

    begin scraper class build

--- lib/whitney_exhibitions/scraper.rb
+++ lib/whitney_exhibitions/scraper.rb
@@ -1,7 +1,11 @@
 class WhitneyExhibitions::Scraper
   
   def scrape_exhibitions
-    
+    uri = "https://whitney.org/exhibitions"
+    doc = Nokogiri::HTML(URI.open(uri))
+    exhibitions = doc.css("div.exhibitions__list h3").text.rstrip
+    binding.pry
+      puts exhibitions
   end
   
 end
